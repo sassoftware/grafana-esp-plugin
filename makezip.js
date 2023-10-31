711bc9a3bdec8225dfeed176253261bd3065fbbb
mtlljm <88196797+mtlljm@users.noreply.github.com>
2023-10-31 09:35:51 +0000

    Create workflow build.yml
    Create a workflow to build and optionally release the plugin.
    Fix - .config/webpack/webpack.config.ts - as it was failing on relative path
    Add makezip.js to make the zip bundles based on the package.json version number

---
+++ makezip.js
@@ -0,0 +1,22 @@
+var zip = require('bestzip');
+var packageJson = require('./package.json');
+
+zip({
+  source: packageJson.name + '/*',
+  destination: './' + packageJson.name + '-' + packageJson.version + '.zip'
+}).then(function() {
+  console.log('Zip complete.');
+}).catch(function(err) {
+  console.error(err.stack);
+  process.exit(1);
+});
+
+zip({
+  source: packageJson.name + '-all' + '/*',
+  destination: './' + packageJson.name + '-' + packageJson.version + '-all' + '.zip'
+}).then(function() {
+  console.log('Zip complete.');
+}).catch(function(err) {
+  console.error(err.stack);
+  process.exit(1);
+});
